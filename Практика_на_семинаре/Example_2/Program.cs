7f2ad89d10291687c70a0779988efd2550afc576
Olga Serbul <Olsen.zika@mail.ru>
2022-10-15 01:21:01 +0300

    commit Example2_мое решение_2_29

---
+++ Практика_на_семинаре/Example_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int sqrNumber = numberB* numberB;
+if(numberA ==sqrNumber)
+ {
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
