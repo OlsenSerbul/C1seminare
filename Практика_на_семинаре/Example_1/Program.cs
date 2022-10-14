ac1156e61815573654960d3b139164a2097b300a
Olga Serbul <Olsen.zika@mail.ru>
2022-10-14 20:54:19 +0300

    commit Example_1 project

---
+++ Практика_на_семинаре/Example_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведение в квадрат
+
+// запрос числа от пользователя
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Квадрат исходного числа
+int sqrNumber = number * number;
+
+// Вывод результата на экран
+Console.WriteLine($"Квадрат вашего числа = {sqrNumber}");
