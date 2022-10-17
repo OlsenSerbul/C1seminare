17d8e6a8d0d0c25784244e9aa29de673fb3dfc92
Olga Serbul <Olsen.zika@mail.ru>
2022-10-17 10:06:19 +0300

    commit Example_3_решение_дни_недели_

---
+++ Практика_на_семинаре/Example_3/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int numberWeek = Convert.ToInt32(Console.ReadLine());
+if (numberWeek>7 || numberWeek<1)
+{
+    Console.WriteLine("Дней недели всего 7. Введите число от 1 до 7");
+}
+else
+{
+    if (numberWeek == 1)
+    {
+        Console.WriteLine("Понедельник");
+    }
+    if (numberWeek == 2)
+    {
+        Console.WriteLine("Вторник");
+    }
+    if (numberWeek == 3)
+    {
+        Console.WriteLine("Среда");
+    }
+    if (numberWeek == 4)
+    {
+        Console.WriteLine("Четверг");
+    }
+    if (numberWeek == 5)
+    {
+        Console.WriteLine("Пятница");
+    }
+    if (numberWeek == 6)
+    {
+        Console.WriteLine("Суббота");
+    }
+    if (numberWeek == 7)
+    {
+        Console.WriteLine("Воскресенье");
+    }
+}
