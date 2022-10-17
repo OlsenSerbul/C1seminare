19b4abaf3c9014ab27e8ee9a8c24897588dd2d90
Olga Serbul <Olsen.zika@mail.ru>
2022-10-17 17:08:42 +0300

    commit Task_4_Определить_все_четные

---
+++ Домашняя_работа/Task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число : ");
+int number =  Convert.ToInt32(Console.ReadLine());
+int X = 1;
+while(X <= number)
+{
+    if(X % 2 > 0)
+    {
+        X = X + 1;
+    }
+    else
+    {
+        Console.WriteLine(X);
+        X = X + 1;
+    }
+}
