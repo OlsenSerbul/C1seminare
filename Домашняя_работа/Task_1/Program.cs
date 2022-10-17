3676c48866980e39732cd0cca480904e1625a768
Olga Serbul <Olsen.zika@mail.ru>
2022-10-17 14:28:14 +0300

    commit Task_1_max_man_numbers

---
+++ Домашняя_работа/Task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB) Console.Write($"MAX = {numberA} , MIN = {numberB} ");
+if(numberA < numberB) Console.WriteLine($"MAX = {numberB} , MIN = {numberA} ");
