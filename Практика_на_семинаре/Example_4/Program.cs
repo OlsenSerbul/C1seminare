2c76f23372af2476d6fe51ab5a7e9b834517c908
Olga Serbul <Olsen.zika@mail.ru>
2022-10-17 14:11:12 +0300

    commit Example_4_цикл_не_доделанно_

---
+++ Практика_на_семинаре/Example_4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int negativN = N * -1;
+
+while(N > negativN)
+{ 
+  if(N < negativN)
+  {
+   Console.WriteLine(N);
+   N = N + 1;
+  }
+  else
+   Console.WriteLine(negativN);
+   negativN = negativN + 1;
+}
+ N = negativN;
+
+
+
